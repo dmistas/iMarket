26d9e7ba86d673a7f62dba0a19fb663cd24102a4
Stanislav Dmitriev <dmistas@gmail.com>
2023-01-26 23:20:47 +0500

    telegram bot logger

---
+++ app/Logging/Telegram/TelegramLoggerHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Logging\Telegram;
+
+use App\Services\Telegram\TelegramBotApi;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+class TelegramLoggerHandler extends AbstractProcessingHandler
+{
+    protected int $chatId;
+    protected string $token;
+
+    public function __construct($config)
+    {
+        $this->chatId = $config['chat_id'];
+        $this->token = $config['token'];
+
+        $level = Logger::toMonologLevel($config['level']);
+
+        parent::__construct($level);
+    }
+
+    protected function write(array $record): void
+    {
+        TelegramBotApi::sendMessage($this->token, $this->chatId, $record['formatted']);
+    }
+}
--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Logging\Telegram\TelegramLoggerFactory;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -116,6 +117,14 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'telegram' => [
+            'driver' => 'custom',
+            'via' => TelegramLoggerFactory::class,
+            'level' => env('LOG_LEVEL', 'debug'),
+            'chat_id' => '-866092374',
+            'token' => '5822368344:AAH8SeVPq8NdHl7EvLAG9dCZkvez8nTJ-FE',
         ],
     ],
 
