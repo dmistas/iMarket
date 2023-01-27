c19f7b173beff6ac7ba9956670f54b76880c73a6
Stanislav Dmitriev <dmistas@gmail.com>
2023-01-27 09:35:19 +0500

    Telegram bot exception

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,16 +2,27 @@
 
 namespace App\Services\Telegram;
 
+use App\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
 
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
+
     public static function sendMessage(string $token, int $chatId, string $message)
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $message,
-        ]);
+        try {
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $message,
+            ]);
+            if ($response->status() != Response::HTTP_OK) {
+                throw new TelegramBotApiException('Telegram service not response');
+            }
+        } catch (TelegramBotApiException$e) {
+            report($e);
+            return false;
+        }
     }
 }
