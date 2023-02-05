0a074600ca741999f305eddd62f22e247151b112
Stanislav Dmitriev <dmistas@gmail.com>
2023-02-05 09:42:18 +0500

    refactor

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -4,24 +4,23 @@ namespace App\Services\Telegram;
 
 use App\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $message)
+    public static function sendMessage(string $token, int $chatId, string $message): bool
     {
         try {
             $response = Http::get(self::HOST . $token . '/sendMessage', [
                 'chat_id' => $chatId,
                 'text' => $message,
-            ]);
-            if ($response->status() != Response::HTTP_OK) {
-                throw new TelegramBotApiException('Telegram service not response');
-            }
-        } catch (TelegramBotApiException$e) {
-            report($e);
+            ])->throw();
+            return $response->successful();
+
+        } catch (Throwable $e) {
+            report(new TelegramBotApiException($e->getMessage()));
             return false;
         }
     }
